5fa39457bc685ffd2aba6b98a4e8b762642dbec4
Shizuka Donaghue <sdonaghue@gmail.com>
2022-10-23 19:33:34 +0000

    Add comments in script.js to describe each section and edit comments in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,25 +28,40 @@ let shuffledQuestions = '';
 let currentQuestionIndex = 0;
 let correctAnswers = 0;
 
+// focus method for the username field so that the cursor is placed in the field
 document.getElementById('username').focus();
 
+/*
+event listeners to call displayInputHelp or closeInputHelp function to display or hide input help 
+for the username field when hovered over the question mark in the form element
+*/
 questionMark.addEventListener('mouseover', displayInputHelp);
 questionMark.addEventListener('mouseleave', closeInputHelp);
 
+/**
+ * displays input help for the username field and hides start and rule buttons
+ */
 function displayInputHelp(){
     inputHelp.classList.remove('hide');
     startButton.classList.add('hide');
     ruleButton.classList.add('hide');    
 }
 
+/**
+ * closes input help for the username field and unhide start and rule buttons
+ */
 function closeInputHelp(){
     inputHelp.classList.add('hide');
     startButton.classList.remove('hide');
     ruleButton.classList.remove('hide'); 
 }
 
+// event listener to call displayRule function to display the rule section
 ruleButton.addEventListener('click', displayRule);
 
+/**
+ * displays rule for the game and hides other elements in the start section
+ */
 function displayRule() {
     rule.classList.remove('hide');
     introductionArea.classList.add('hide');
@@ -55,8 +70,12 @@ function displayRule() {
     ruleButton.classList.add('hide');
 }
 
+// event listener to call closeRule function to close the rule section
 document.getElementById('close-rule-button').addEventListener('click', closeRule);
 
+/**
+ * closes rule for the game and unhide other elements in the start section
+ */
 function closeRule() {
     rule.classList.add('hide');
     introductionArea.classList.remove('hide');
@@ -66,12 +85,21 @@ function closeRule() {
     document.getElementById('username').focus();
 }
 
-// Wait for the DOM to finish loading before running the game so that all fuctions will work
-// Add event listerner to the start button in the form element
+/*
+wait for the DOM to finish loading before running the game so that all functions for the game will work,
+add "submit" event listerner to the form element to call runGame function only when a username that 
+meets the criteria set in index.html (a min 3 and a max of 20 characters) 
+*/
 document.addEventListener('DOMContentLoaded', function() {
     nameInputForm.addEventListener('submit', runGame); 
 });
 
+/**
+ * stores the username in "user" variable, 
+ * hides the start section and displayes the game section, 
+ * shuffles the array of questions and stores it in "currentQuestionSet" variables, 
+ * calls "nextQuestion" function
+ */
 function runGame(){
     user = document.getElementById('username').value;
     document.getElementById('start-section').classList.add('hide');
@@ -85,6 +113,15 @@ function runGame(){
     nextQuestion();    
 }
 
+/**
+ * preparation for the next question, 
+ * hides answer explanation text from the previous question, 
+ * calculates and displays the question number, 
+ * reactivates the answer buttons and cursor pointers, 
+ * removes class indicating correct or incorrect from the answer buttons, 
+ * hides the next button so that it requires the user to select an answer before they can move forward, 
+ * call displayQuestion function
+ */
 function nextQuestion(){
     explanationText.classList.add('hide');
     currentQuestionNumber.innerText = currentQuestionIndex + 1;
@@ -108,8 +145,10 @@ function nextQuestion(){
     displayQuestion();
 }
 
-
-//  displayQuestion function loops through questions array and display each question and possible answers
+/**
+ * loops through the questions array and display each question and possible answers, 
+ * once the answer button is selected, call checkAnswer function
+ */
 function displayQuestion(){
     for (let i = 0; i < questions.length; i++) {
         questionText.innerHTML = currentQuestionSet[currentQuestionIndex].question;
@@ -125,6 +164,16 @@ function displayQuestion(){
     }
 }
 
+/**
+ * disables the answer buttons once an answer has been selected so that the user can only select an answer once, 
+ * disables the cursor pointer effect once the answer buttons are disabled,
+ * checks the user answer and if it is correct, increments the correct answer score,
+ * shows the answer explanation regardless of whether the answer is correct or not, 
+ * adds class "correct" to the answer button if the answer is correct or "incorrect" if the answer is incorrect, 
+ * if there are more questions left in the array, displays the next button so that the user can move forward to the 
+ * next question and calls nextQuestion function, 
+ * if there are no more questions left in the array, displays the finish button
+ */
 function checkAnswer(){
     choice1.setAttribute('disabled', 'disabled');
     choice2.setAttribute('disabled', 'disabled');
@@ -156,25 +205,34 @@ function checkAnswer(){
             this.classList.add('incorrect');
         }        
     }
-
-    nextButton.classList.remove('hide');
-    
+ 
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         currentQuestionIndex++;  
+        nextButton.classList.remove('hide');
         nextButton.addEventListener('click', nextQuestion);
     } else {
-        nextButton.classList.add('hide');
         finishButton.classList.remove('hide');     
     }   
 }
 
+/**
+ * obtain the previous score for correct answers and increments it if the answer selected is correct and 
+ * stores the new score
+ */
 function incrementScore(){
     let previousScore = parseInt(document.getElementById('correct-answers').innerText);
     document.getElementById('correct-answers').innerText = ++previousScore;
 }
 
+// event listener for the finish button to call result function
 finishButton.addEventListener('click', result);
 
+/**
+ * hides the game section and displays the result section, 
+ * displays the username stored and the number of correct answers and the final score, 
+ * displays different messages depending on the final score, 
+ * event listener for the play again button to call resetGame function
+ */
 function result(){
     gameSection.classList.add('hide');
     resultSection.classList.remove('hide');
@@ -204,6 +262,11 @@ function result(){
     document.getElementById('play-again').addEventListener('click', resetGame);
 }
 
+/**
+ * resets the question index, the number of correct answers and scores, 
+ * hides the result section and the finish button and displays the next button, 
+ * call runGame function to start the game again
+ */
 function resetGame() {
     currentQuestionIndex = 0;
     correctAnswers = 0;
